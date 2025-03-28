7a11152310d8df27675a124253f8d08b1b689f67
ALEXX4891 <alexx4891@mail.ru>
2025-03-28 21:23:10 +0500

    добавит ограничения на количество запросов

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware(['throttle:myApi'])->group(function () {
     Route::apiResource('categories', \App\Http\Controllers\Api\V1\CategoryController::class);
-});
-
-Route::prefix('v1')->group(function () {
     Route::apiResource('posts', \App\Http\Controllers\Api\V1\PostController::class);
 });
